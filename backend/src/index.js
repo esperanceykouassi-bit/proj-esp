ea8f9cc79c63686da53e2ec64de88450b211cfef
esperanceykouassi-bit <esperanceykouassi@gmail.com>
2026-07-13 13:38:04 +0200

    fix: ajout de l'url frontend dans la configuration cors du backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,9 +13,12 @@ const PORT = process.env.PORT || 3001
 
 app.use(helmet())
 app.use(cors({
-  origin:      process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-}))
+  origin: [
+    'http://localhost:5173',          // Pour vos tests sur PC
+    'https://proj-esp-1.onrender.com' // Pour votre site en ligne
+  ],
+  credentials: true
+}));
 app.use(express.json())
 
 const globalLimiter = rateLimit({
